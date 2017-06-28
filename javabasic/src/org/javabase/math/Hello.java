1149c5ca2abc73d8e930efff80b2fa75b89a762f
chinazzl <coderzzl@163.com>
2017-06-28 15:40:57 +0800

    提交github

---
+++ javabasic/src/org/javabase/math/Hello.java
@@ -0,0 +1,10 @@
+package org.javabase.math;
+
+/**
+ * Created by Felix on 2017/6/28.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("First commit");
+    }
+}
