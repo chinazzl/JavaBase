34a47196ba63d99e9374b12b1386f85680a88aa1
zhangzhaolin <coderzzl@163.com>
2018-06-22 11:10:12 +0800

    一些更改

---
+++ javabasic/src/langer/IntegerTest.java
@@ -0,0 +1,22 @@
+package langer;
+
+import java.util.HashMap;
+
+/**
+ * Integer的自动装箱
+ *
+ * Integer的比较
+ */
+public class IntegerTest {
+    public static void main(String[] args) {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        int i = Object.class.hashCode();
+
+        Integer a = 0;
+
+    }
+
+
+}
+
+
