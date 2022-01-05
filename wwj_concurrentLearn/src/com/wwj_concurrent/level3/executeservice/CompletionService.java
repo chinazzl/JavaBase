08e25c7eccba152e0b1a40fe76608ab1a62566e8
zzl <coderzzl@163.com>
2022-01-05 17:22:27 +0800

    CompletionService

---
+++ wwj_concurrentLearn/src/com/wwj_concurrent/level3/executeservice/CompletionService.java
@@ -0,0 +1,10 @@
+package com.wwj_concurrent.level3.executeservice;
+
+/**
+ * @author Julyan
+ * @version V1.0
+ * @Description: 补全Future的缺陷
+ * @Date: 2022/1/5 16:55
+ */
+public class CompletionService {
+}
