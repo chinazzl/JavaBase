20e891d4c651dc7fb4ee4962062e96a369f90ebe
zzl <coderzzl@163.com>
2022-03-18 17:48:28 +0800

    drools simple demo, drools 7.66.0

---
+++ simpleDrools/src/main/java/org/testdrools/entity/Student.java
@@ -0,0 +1,29 @@
+package org.testdrools.entity;
+
+/**
+ * @author Julyan
+ * @version V1.0
+ * @Description:
+ * @Date: 2022/3/18 17:02
+ */
+public class Student {
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- simpleDrools/src/test/java/SimpleDrools.java
+++ simpleDrools/src/test/java/SimpleDrools.java
@@ -1,3 +1,7 @@
+import org.drools.core.base.RuleNameEndsWithAgendaFilter;
+import org.drools.core.base.RuleNameEqualsAgendaFilter;
+import org.drools.core.base.RuleNameMatchesAgendaFilter;
+import org.drools.core.base.RuleNameStartsWithAgendaFilter;
 import org.junit.Test;
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
@@ -7,6 +11,7 @@ import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.testdrools.entity.ComparationOperation;
 import org.testdrools.entity.Order;
+import org.testdrools.entity.Student;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +19,7 @@ import java.util.List;
 /**
  * @author Julyan
  * @version V1.0
- * @Description:
+ * @Description: Bean先加入到工作内存变成fact对象 => 和规则库加载 => 规则库和fact对象匹配 => 匹配成功的加入Agenda 议程 => 然后执行HRS
  * @Date: 2022/3/16 14:38
  */
 public class SimpleDrools {
@@ -42,6 +47,9 @@ public class SimpleDrools {
         System.out.println("优惠后的价格 " + order.getRealPrice());
     }
 
+    /**
+     * 使用比较语法
+     */
     @Test
     public void  testCompareOper() {
         KieServices kieServices = KieServices.Factory.get();
@@ -55,6 +63,50 @@ public class SimpleDrools {
         kieSession.insert(comparationOperation);
         int i = kieSession.fireAllRules();
         System.out.println("扫描规则共 " + i + "个");
+        kieSession.dispose();
+    }
+
+    // 指定规则进行匹配
+    @Test
+    public void testAppointRules() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieSession kieSession = kieServices.getKieClasspathContainer().newKieSession();
+        ComparationOperation comparationOperation = new ComparationOperation();
+        comparationOperation.setUserName("sb");
+        List<String> data = new ArrayList<>();
+        data.add("zs");
+        data.add("ls");
+        comparationOperation.setData(data);
+        kieSession.insert(comparationOperation);
+        // 指定具体的规则名字进行匹配
+//        int i = kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("comparationMatched"));
+        // 指定前缀、后缀、正则。
+        // 后缀
+        int i = kieSession.fireAllRules(new RuleNameEndsWithAgendaFilter("Matched"));
+        // 前缀
+//        kieSession.fireAllRules(new RuleNameStartsWithAgendaFilter("comparation"));
+        // 正则
+//        kieSession.fireAllRules(new RuleNameMatchesAgendaFilter("comparation.*"));
+
+        System.out.println("扫描规则共 " + i + "个");
+        kieSession.dispose();
+    }
+
+    // 内置方法
+    @Test
+    public void testUpdateRule() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieSession kieSession = kieServices.getKieClasspathContainer().newKieSession();
+        Student student = new Student();
+        student.setAge(5);
+        kieSession.insert(student);
+        int i = kieSession.fireAllRules();
+        System.out.println("扫描规则共 " + i + "个");
+
+        student.setAge(15);
+        kieSession.insert(student);
+        int m= kieSession.fireAllRules();
+        System.out.println("扫描规则共 " + m + "个");
         kieSession.dispose();
     }
 }
