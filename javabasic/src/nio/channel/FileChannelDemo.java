0263bb240b1ef14f653be7d7415c5d2d437e08b5
zzl <coderzzl@163.com>
2022-07-06 23:16:54 +0800

    Nio channel API

--- javabasic/src/nio/channel/FileChannelDemo.java
+++ javabasic/src/nio/channel/FileChannelDemo.java
@@ -30,9 +30,52 @@ public class FileChannelDemo {
     }
 
     public static void main(String[] args) {
-        baseReader();
+        fileReader2();
+//        baseReader();
+//        fileReader();
 //        fileWrite();
 //        fileSyncWriter();
+    }
+
+    /**
+     * 验证int read(ByteBuffer dst) 方法将字节放入ByteBuffer当前位置
+     */
+    private static void fileReader2() {
+        // ababcd
+        try (FileInputStream fileInputStream = new FileInputStream(new File(logPath));
+             FileChannel fileChannel = fileInputStream.getChannel();) {
+            // 从索引2的位置开始读
+            fileChannel.position(2);
+            ByteBuffer byteBuffer = ByteBuffer.allocate(6);
+            // byteBuffer 从索引3 的地方开始写入
+            byteBuffer.position(3);
+            fileChannel.read(byteBuffer);
+            byte[] array = byteBuffer.array();
+            for (byte b : array) {
+                if (b == 0) {
+                    System.out.print("空格\t");
+                } else {
+                    System.out.print((char) b);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fileReader() {
+        try (FileInputStream fileInputStream = new FileInputStream(new File(logPath));
+             FileChannel fileChannel = fileInputStream.getChannel()) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(5);
+            int read = fileChannel.read(byteBuffer);
+            System.out.println(read);
+            byteBuffer.clear();
+            // 再次执行read 返回值为0，因为byteBuffer中没有remaining空间
+            System.out.println(fileChannel.read(byteBuffer));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -40,13 +83,13 @@ public class FileChannelDemo {
      */
     private static void baseReader() {
         try (InputStream inputStream = Files.newInputStream(new File(logPath).toPath());
-            OutputStream outputStream = Files.newOutputStream(new File(targetPath).toPath())) {
+             OutputStream outputStream = Files.newOutputStream(new File(targetPath).toPath())) {
             int len = -1;
             byte[] bytes = new byte[1024];
             String out = null;
             while ((len = inputStream.read(bytes)) > 0) {
-                out += new String(bytes,0,len);
-                outputStream.write(bytes,0,len);
+                out += new String(bytes, 0, len);
+                outputStream.write(bytes, 0, len);
             }
             System.out.println(out);
         } catch (Exception e) {
@@ -84,7 +127,7 @@ public class FileChannelDemo {
      */
     private static void fileSyncWriter() {
         try (FileOutputStream fosRef = new FileOutputStream(logPath);
-            FileChannel fileChannel = fosRef.getChannel();) {
+             FileChannel fileChannel = fosRef.getChannel();) {
             for (int i = 0; i < 10; i++) {
                 Thread t1 = new Thread(() -> {
                     ByteBuffer buffer = ByteBuffer.wrap("abcde\n".getBytes(StandardCharsets.UTF_8));
