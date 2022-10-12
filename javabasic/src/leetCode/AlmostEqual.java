1ea389fdc0d6ec48cc4c54f8a8da4f31bb237b06
zzl <coderzzl@163.com>
2022-10-12 23:25:15 +0800

    leetcode 1790

--- javabasic/src/leetCode/AlmostEqual.java
+++ javabasic/src/leetCode/AlmostEqual.java
@@ -10,18 +10,46 @@ package leetCode;
  **********************************/
 public class AlmostEqual {
     public static void main(String[] args) {
+        // bank kanb true
+        // ab ba true
+        // ac aa false
+        //adsfasdf ffsadf false
         System.out.println(areAlmostEqual("bank", "kanb"));
+        System.out.println(areAlmostEqual("ab", "ba"));
+        System.out.println(areAlmostEqual("ac", "aa"));
+        System.out.println(areAlmostEqual("asdfasdf", "kasdfsdfsdanb"));
+        System.out.println(areAlmostEqual("a", "b"));
+        System.out.println(areAlmostEqual("kelb", "kelb"));
     }
+
+    /**
+     * 思路：
+     * 1. 遍历两个字符如果字符都相等，则返回true
+     * 2. 如果不相等则最多一次交换肯定是最多两个字符不同并且s2[j] = s1[i]; s1[j] = s2[i]
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
     public static boolean areAlmostEqual(String s1, String s2) {
         int count = 0;
+        int m = -1;
+        int n = -1;
         for (int i = 0; i < s1.length(); i++) {
+            // 获取两个交换的字符的角标
             if (s1.charAt(i) != s2.charAt(i)) {
                 count++;
-            }
-            if (count >= s1.length() / 2) {
-                return false;
+                if (m == -1) {
+                    m = i;
+                }
+                if (n == -1 && m != i) {
+                    n = i;
+                }
             }
         }
-        return true;
+        if ( count == 0) {
+            return true;
+        }
+        return count == 2 && s1.charAt(m) == s2.charAt(n) && s2.charAt(m) == s1.charAt(n);
     }
 }
