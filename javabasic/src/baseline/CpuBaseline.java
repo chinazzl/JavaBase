7fbe6fdb3c766cacdfd105da63f2faafc0c1635d
zzl <coderzzl@163.com>
2023-02-23 00:05:39 +0800

    sliding window

--- javabasic/src/baseline/CpuBaseline.java
+++ javabasic/src/baseline/CpuBaseline.java
@@ -2,6 +2,7 @@ package baseline;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
+import net.bytebuddy.asm.Advice;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -10,11 +11,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.StringJoiner;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**********************************
  * @author zhang zhao lin
@@ -62,36 +65,80 @@ public class CpuBaseline {
     @Test
     public void putNumberIntoRanged() {
         // 采集一个ip 一个月内的当前时间同一时刻的cpu使用率的性能数据。
-        // Double[] doubles = new Double[]{9.18, 9.64, 8.53, 9.25, 9.01, 9.35, 9.48, 6.62, 6.26, 5.37, 6.97, 9.02, 7.13, 6.57, 9.03,
-        //         9.65, 9.50, 9.91, 7.24, 5.65, 7.72, 9.73, 6.98, 9.94, 8.36, 6.66, 8.93, 5.90, 9.31, 7.20, 0.12, 2.32};
-        Double[] doubles = CPUsage.cpu_218_193_0045;
+         Double[] doubles = new Double[]{9.18, 9.64, 8.53, 9.25, 9.01, 9.35, 9.48, 6.62, 6.26, 5.37, 6.97, 9.02, 7.13, 6.57, 9.03,
+                 9.65, 9.50, 9.91, 7.24, 5.65, 7.72, 9.73, 6.98, 9.94, 8.36, 6.66, 8.93, 5.90, 9.31, 7.20, 0.12, 2.32};
+        //Double[] doubles = CPUsage.cpus_real2;
         List<Double> c1 = Arrays.asList(doubles);
         List<Double> cpuDatas = new ArrayList<>(c1);
         // 获取这段时间性能数据的最大值
         Double maxCPU = cpuDatas.stream().max(Double::compareTo).get();
         // 根据样本最大值计算五个区间范围
-        long v = Math.round((maxCPU / 5));
+        long v = Math.round((maxCPU / 5)) == 0 ? 1 : Math.round((maxCPU / 5));
         // 将时间段的性能数据分别分布在不同的区间[0,0+v], [0+v+0.01, 0+v+0.01+v] ...
-        Multimap<String, Double> rangeMap = ArrayListMultimap.create();
-        for (Double cpu : cpuDatas) {
-            int start = 0, rangeName = 1;
-            double rangeStart = 0.0;
-            while (rangeStart < Math.ceil(maxCPU)) {
-                if (cpu >= rangeStart + 0.01 && cpu <= (int) (rangeStart + v)) {
-                    rangeMap.put(String.valueOf(rangeName), cpu);
-                    break;
-                }
-                rangeStart = rangeStart + v;
-                rangeName++;
+        //Multimap<String, Double> rangeMap = ArrayListMultimap.create();
+        //for (Double cpu : cpuDatas) {
+        //    int start = 0, rangeName = 1;
+        //    double rangeStart = 0.0;
+        //    while (rangeStart < Math.ceil(maxCPU)) {
+        //        if (cpu >= rangeStart + 0.01 && cpu <= (int) (rangeStart + v)) {
+        //            rangeMap.put(String.valueOf(rangeName), cpu);
+        //            break;
+        //        }
+        //        rangeStart = rangeStart + v;
+        //        rangeName++;
+        //    }
+        //}
+        List<Double> sorted = cpuDatas.stream().sorted(Double::compareTo).collect(Collectors.toList());
+        List<Double[]> container = new ArrayList<Double[]>();
+        List<Double[]> ranger = ranger(container, sorted, v);
+        // ===============================
+        System.out.println("进行区间划分");
+        for (Double[] ds : ranger) {
+            for (Double d : ds) {
+                System.out.print(d + ",");
             }
+            System.out.println();
         }
-        System.out.println(rangeMap);
+        // =================================================================
         // 获取样本数据，取分布不同区间数据量最多的前三个区间数据作为样本区间
-        List<Double> collect = rangeMap.asMap().entrySet().stream().map(m -> m.getValue())
-                .sorted(Comparator.comparingInt(Collection::size))
-                .limit((int) Math.floor(rangeMap.asMap().size() >> 1)).flatMap(c -> c.stream()).collect(Collectors.toList());
+        //LinkedList<Collection<Double>> collect = rangeMap.asMap().entrySet().stream().map(m -> m.getValue())
+        //        .sorted((m1, m2) -> Integer.compare(m2.size(), m1.size()))
+        //        .collect(Collectors.toCollection(LinkedList::new));
+        System.out.println("区间排序");
+        List<Double[]> collect = ranger.stream()
+                .sorted((m1, m2) -> Integer.compare(m1.length, m2.length))
+                .collect(Collectors.toList());
+        // ===============================
+        System.out.println("进行区间划分");
+        for (Double[] ds : collect) {
+            for (Double d : ds) {
+                System.out.print(d + ",");
+            }
+            System.out.println();
+        }
+        // =================================================================
+        Iterator<Double[]> iterator = collect.iterator();
+        List<Double[]> useList = new ArrayList<Double[]>();
+        while (iterator.hasNext()) {
+            Double[] next = iterator.next();
+            if (next.length== 1) {
+                break;
+            }
+            iterator.remove();
+            useList.add(next);
+        }
+        List<Double> collect1 = collect.stream().flatMap(Stream::of).collect(Collectors.toList());
         // 移除不需要的样本数据，在这里只需要后三个的区间的数据作为样本数据
-        cpuDatas.removeAll(collect);
+        cpuDatas.removeAll(collect1);
+        if (useList.size() != 1 && useList.size() > (int) Math.floor(ranger.size() >> 1)) {
+            List<Double> collect2 = useList.stream()
+                    .sorted((m1, m2) -> Integer.compare(m2.length, m1.length))
+                    .skip((int) Math.floor(ranger.size() >> 1))
+                    .flatMap(Stream::of).collect(Collectors.toList());
+            // 移除不需要的样本数据，在这里只需要后三个的区间的数据作为样本数据
+            cpuDatas.removeAll(collect2);
+        }
+        System.out.println(cpuDatas);
         // 滑动窗口，分别计算1~24  2~25 3~ 26 以此类推的标准差
         // 开始计算标准差
         // Double reduce = window.stream().reduce(0.0, Double::sum);
@@ -147,4 +194,43 @@ public class CpuBaseline {
         }
         return resList;
     }
+
+    /**
+     * 区间取数法
+     */
+    @Test
+    public void testRangeArray() {
+        Double[] doubles = new Double[]{9.18, 9.64, 8.53, 9.25, 9.01, 9.35, 9.48, 6.62, 6.26, 5.37, 6.97, 9.02, 7.13, 6.57, 9.03,
+                9.65, 9.50, 9.91, 7.24, 5.65, 7.72, 9.73, 6.98, 9.94, 8.36, 6.66, 8.93, 5.90, 9.31, 7.20, 0.12, 2.32};
+        double maxCPU = Stream.of(doubles).max(Double::compareTo).get();
+        // 根据样本最大值计算五个区间范围 2
+        long v = Math.round((maxCPU / 5)) == 0 ? 1 : Math.round((maxCPU / 5));
+        List<Double> sortCpus = Stream.of(doubles).sorted(Double::compareTo).collect(Collectors.toList());
+        List<Double[]> container = new ArrayList<Double[]>();
+        List<Double[]> ranger = ranger(container, sortCpus, v);
+        for (Double[] ds : ranger) {
+            for (Double d : ds) {
+                System.out.print(d + ",");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private List<Double[]> ranger(List<Double[]> container, List<Double> ds, long k) {
+        Double[] d = new Double[ds.size()];
+        Iterator<Double> iterator = ds.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            Double next = iterator.next();
+            d[i++] = next;
+            iterator.remove();
+            if (next >=  k) {
+                ranger(container, ds, 2 * k);
+            }
+        }
+        container.add(d);
+        return container;
+    }
+
 }
