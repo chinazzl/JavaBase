fbf2e3e735421c26cd5b034b3d489934a1e08906
zzl <coderzzl@163.com>
2023-04-26 23:41:46 +0800

    sliding window

---
+++ javabasic/src/leetCode/Leetcode1031.java
@@ -0,0 +1,37 @@
+package leetCode;
+
+/**********************************
+ * @author zhang zhao lin
+ * @date 2023年04月26日 22:30
+ * @Description: 给你一个整数数组 nums 和两个整数 firstLen 和 secondLen，请你找出并返回两个非重叠 子数组 中元素的最大和，长度分别为 firstLen 和 secondLen 。
+ * 长度为 firstLen 的子数组可以出现在长为 secondLen 的子数组之前或之后，但二者必须是不重叠的。
+ * 子数组是数组的一个 连续 部分。
+ **********************************/
+public class Leetcode1031 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 输入：nums = [0,6,5,2,2,5,1,9,4], firstLen = 1, secondLen = 2
+     * 输出：20
+     * 解释：子数组的一种选择中，[9] 长度为 1，[6,5] 长度为 2。
+     * <p>
+     * 输入：nums = [3,8,1,3,2,1,8,9,0], firstLen = 3, secondLen = 2
+     * 输出：29
+     * 解释：子数组的一种选择中，[3,8,1] 长度为 3，[8,9] 长度为 2。
+     * <p>
+     * 输入：nums = [2,1,5,6,0,9,5,0,3,8], firstLen = 4, secondLen = 3
+     * 输出：31
+     * 解释：子数组的一种选择中，[5,6,0,9] 长度为 4，[0,3,8] 长度为 3。
+     *
+     * @param nums
+     * @param firstLen
+     * @param secondLen
+     * @return
+     */
+    public static int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
+
+    }
+}
