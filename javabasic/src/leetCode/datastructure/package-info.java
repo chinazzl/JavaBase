884d9c23b0cdb1ac76a4269a3aea10eccbe79791
zzl <coderzzl@163.com>
2023-11-17 15:33:12 +0800

    init package

---
+++ javabasic/src/leetCode/datastructure/package-info.java
@@ -0,0 +1,6 @@
+/**********************************
+ * @author zhang zhao lin
+ * @date 2023年11月17日 15:30
+ * @Description: 数据结构专题
+ **********************************/
+package leetCode.datastructure;
